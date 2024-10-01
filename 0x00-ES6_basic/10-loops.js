6ecad6cca3cc66b074e0767bd009b76176d23508
tevn23 <mabulatt23@gmail.com>
2024-10-01 06:05:50 +0200

    Added Task 10 -12

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [index, value] of array.entries()) {
+    array[index] = appendString + value;
+  }
+
+  return array;
+}
+
