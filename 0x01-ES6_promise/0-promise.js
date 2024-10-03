0dc9eaad90d64ee693f80c1738c11e6e941bd4ee
tevn23 <mabulatt23@gmail.com>
2024-10-03 04:36:30 +0200

    ESLint compliant code

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,5 @@
 export default function getResponseFromAPI() {
+
   return new Promise((resolve, reject) => {
     // Simulating an API call or asynchronous operation
     resolve('Success'); // You can resolve with any value you like
