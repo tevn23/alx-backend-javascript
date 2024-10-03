728aea578ba96ee270a83f03fe193b2a01ebf321
tevn23 <mabulatt23@gmail.com>
2024-10-03 05:06:01 +0200

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,6 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([body, fullName]) => 
-      console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`)
-    )
+    .then(([body, fullName]) => console.log(`${body.body} ${fullName.firstName} ${fullName.lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
