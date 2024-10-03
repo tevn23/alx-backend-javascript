8712733dab53afb61c695ae98f624c460b59780b
tevn23 <mabulatt23@gmail.com>
2024-10-03 04:14:15 +0200

    Task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an API call or asynchronous operation
+    resolve('Success'); // You can resolve with any value you like
+  });
+}
+
