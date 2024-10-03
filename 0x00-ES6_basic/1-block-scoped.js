88ed808da6d5b1b587878214b42fe02c8c5de8d4
tevn23 <mabulatt23@gmail.com>
2024-10-03 04:33:47 +0200

    ESLint compliant code

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // This task is now block-scoped
-    let task2 = false; // This task2 is also block-scoped
+    const task = true; // This task is now block-scoped
+    const task2 = false; // This task2 is also block-scoped
   }
 
   return [task, task2];
 }
-
